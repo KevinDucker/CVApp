b2a0ae8cf46c74c96a25ea39cc20c125a8769c33
Kevin Ducker <kevin.ducker@globant.com>
2019-05-24 16:56:12 -0500

    Initial commit

---
+++ CVApp/CVConnectionHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CVConnectionHelper.swift
+//  CVApp
+//
+//  Created by Kevin Ducker Marin on 5/24/19.
+//  Copyright © 2019 Kevin Ducker Marin. All rights reserved.
+//
+
+import Foundation
---
+++ CVApp/CVModels.swift
@@ -0,0 +1,9 @@
+//
+//  CVModels.swift
+//  CVApp
+//
+//  Created by Kevin Ducker Marin on 5/24/19.
+//  Copyright © 2019 Kevin Ducker Marin. All rights reserved.
+//
+
+import Foundation
