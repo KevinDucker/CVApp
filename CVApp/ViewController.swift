9742ceec79348ffa3a3d57734f54939f35e203c8
Kevin Ducker <kevin.ducker@globant.com>
2019-05-24 13:42:03 -0500

    Initial Commit

---
+++ CVApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CVApp
+//
+//  Created by Kevin Ducker Marin on 5/24/19.
+//  Copyright © 2019 Kevin Ducker Marin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
