432fe456a4f8c68c045566d0a6c61883ee3981a8
Kevin Ducker <kevin.ducker@globant.com>
2019-05-25 11:57:13 -0500

    Mark added to unit test

--- CVAppTests/CVAppTests.swift
+++ CVAppTests/CVAppTests.swift
@@ -7,12 +7,12 @@ import XCTest
 
 class CVAppConnectionTests: XCTestCase, CVAPIDataManagerToErrorHelperProtocol {
 
+  // MARK: - Variables
   let goodUrl = "https://gist.githubusercontent.com/KevinDucker/7e0656d0bfeb59d125ace3f07082db1d/raw/3e60b3d8a1178bd6f50d70a097c9379396476887/CVJson.json"
   let badJsonFileUrl = "https://gist.githubusercontent.com/KevinDucker/399a14a581f8f210a64ef0aa2fb320e1/raw/0076338093a108e63033cd9093ebe92e0acc58af/CVJsonBad"
   let badUrl = "https://gist.githubusercontent.com/KevinDucker//raw/CVJson.json"
 
   var view: CVErrorHelperToViewControllerProtocol?
-
   var expectation = XCTestExpectation()
 
   // MARK: - Test functions
